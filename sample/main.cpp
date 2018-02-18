841ef32927ac4b150828b2aa7e781307313ed0d5
tolik <talamtolik@gmail.com>
2018-02-18 12:11:16 +0300

    delete show input and output array on display

--- sample/main.cpp
+++ sample/main.cpp
@@ -19,16 +19,12 @@ int main(int argc, char* argv[]){
 							return distribution(generator);
 							});
 				 		
-	copy(test_data.begin(), test_data.end(), ostream_iterator<int>(cout, " "));
 	cout << endl;	
 	auto start = Time::now();
 	shell_sort(test_data.begin(), test_data.end());
 	auto end = Time::now();
 	fms elapsed_time = end - start;
 	cout << elapsed_time.count() << "ms" << endl;
-	
-	copy(test_data.begin(), test_data.end(), ostream_iterator<int>(cout, " "));
-	
 	return 0;
 }		
 
