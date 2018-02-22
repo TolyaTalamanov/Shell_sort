ab1fd839a99f26190b76187f0777267998f5796f
tolik <talamtolik@gmail.com>
2018-02-22 18:35:09 +0300

    added the ability to set the size of the test data

--- test_generator/test_generator.cpp
+++ test_generator/test_generator.cpp
@@ -12,31 +12,34 @@ void writeDataToFile(vector<int> data, const string& file_name);
 int main(int argc, char* argv[]){
 
 	const int count_tests = 7;
+	const int default_size = 10;
+	const int data_size = argv[1] ? atoi(argv[1]) : default_size;
+
 	vector<vector<int>> test_data(count_tests);
 	
-	test_data[0].resize(10);
+	test_data[0].resize(data_size);
 	fill(test_data[0].begin(), test_data[0].end(), 5);
 
-	test_data[1].resize(10);
+	test_data[1].resize(data_size);
 	iota(test_data[1].begin(), test_data[1].end(), 0);
 
-	test_data[2].resize(10);
+	test_data[2].resize(data_size);
 	iota(test_data[2].rbegin(), test_data[2].rend(), 0);
 	
     mt19937 generator{random_device()()};
     uniform_int_distribution<int> distribution(numeric_limits<int>::min(),
                                                numeric_limits<int>::max());
-	test_data[3].resize(10);
+	test_data[3].resize(data_size);
     generate(test_data[3].begin(), test_data[3].end(), [&distribution, &generator](){
         return distribution(generator);
     });
 
-	test_data[4].resize(10);
+	test_data[4].resize(data_size);
 	generate(test_data[4].begin(), test_data[4].end(), [&generator, &distribution](){
 				return abs(distribution(generator));
 			});
 
-	test_data[5].resize(10);
+	test_data[5].resize(data_size);
 	generate(test_data[5].begin(), test_data[5].end(), [&generator, &distribution](){
 				int random_val = distribution(generator);
 				return random_val < 0 ? random_val : -1 * random_val; 
