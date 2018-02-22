229301360af9b92f7c4c124a261f435a5ea6b9b7
tolik <talamtolik@gmail.com>
2018-02-22 12:24:33 +0300

    write test data to file

--- test_generator/test_generator.cpp
+++ test_generator/test_generator.cpp
@@ -2,11 +2,17 @@
 #include<random>
 #include<vector>
 #include<algorithm>
+#include<fstream>
 #include<iterator>
+#include<string>
 using namespace std;
 
+void writeDataToFile(vector<int> data, const string& file_name);
+
 int main(int argc, char* argv[]){
-	vector<vector<int>> test_data(7);
+
+	const int count_tests = 7;
+	vector<vector<int>> test_data(count_tests);
 	
 	test_data[0].resize(10);
 	fill(test_data[0].begin(), test_data[0].end(), 5);
@@ -17,10 +23,9 @@ int main(int argc, char* argv[]){
 	test_data[2].resize(10);
 	iota(test_data[2].rbegin(), test_data[2].rend(), 0);
 	
-	int seed;
-    mt19937 generator(seed);
-    uniform_int_distribution<int> distribution(std::numeric_limits<int>::min(),
-                                               std::numeric_limits<int>::max());
+    mt19937 generator{random_device()()};
+    uniform_int_distribution<int> distribution(numeric_limits<int>::min(),
+                                               numeric_limits<int>::max());
 	test_data[3].resize(10);
     generate(test_data[3].begin(), test_data[3].end(), [&distribution, &generator](){
         return distribution(generator);
@@ -38,18 +43,15 @@ int main(int argc, char* argv[]){
 			});
 	test_data[6].resize(1);
 	
-	copy(test_data[0].begin(), test_data[0].end(), ostream_iterator<int>(cout, " "));
-	cout << endl;
-	copy(test_data[1].begin(), test_data[1].end(), ostream_iterator<int>(cout, " "));
-	cout << endl;
-	copy(test_data[2].begin(), test_data[2].end(), ostream_iterator<int>(cout, " "));
-	cout << endl;
-	copy(test_data[3].begin(), test_data[3].end(), ostream_iterator<int>(cout, " "));
-	cout << endl;
-	copy(test_data[4].begin(), test_data[4].end(), ostream_iterator<int>(cout, " "));
-	cout << endl;
-	copy(test_data[5].begin(), test_data[5].end(), ostream_iterator<int>(cout, " "));
-	cout << endl;
+	for(int i = 0; i < count_tests; ++i){
+		writeDataToFile(test_data[i], "/home/tolik/project/shell_sort/test_generator/test_data/data_" + 
+										to_string(i) + ".bin");
+	}
 
 	return 0;
+}
+void writeDataToFile(vector<int> data, const string& file_path){
+	ofstream file(file_path);
+	ostream_iterator<int> file_iterator(file, " ");
+	copy(data.begin(), data.end(), file_iterator);
 }
