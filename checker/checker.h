75462340712b04dc0bcbad1a39f15d15295c1579
tolik <talamtolik@gmail.com>
2018-02-25 01:19:49 +0300

    fix log file path and add data dir path

--- checker/checker.h
+++ checker/checker.h
@@ -8,8 +8,9 @@ enum verdict { NO = 1, AC, WA, CE, ML, TL, RE, IL, PE, DE };
 
 class Checker{
 	private:
+		const std::string log_file_path_ = "../checker/checker_log.txt";
 		std::ofstream log_file_;
-		const std::string log_file_path_ = "../../shell_sort/checker/checker_log.txt";
+		std::string data_dir_path_ = "../checker/";
 
 		enum class log_params_ { NO = 1, VERDICT, MESSAGE, TIME, MEMORY };
 
@@ -22,7 +23,6 @@ class Checker{
 			//write_type(log_params_::VERDICT);
 			log_file_ << v;
 		}
-
 		void write_message(std::string message){
 			//write_type(log_params_::MESSAGE);
 			log_file_ << message;
