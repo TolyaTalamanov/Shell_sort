d81a0be0ef14bab660506cc97c1dfb6e30b5254d
tolik <talamtolik@gmail.com>
2018-02-25 11:30:02 +0300

    swap public and private

--- checker/checker.h
+++ checker/checker.h
@@ -7,26 +7,6 @@
 enum verdict { NO = 1, AC, WA, CE, ML, TL, RE, IL, PE, DE };
 
 class Checker{
-	private:
-		const std::string log_file_path_ = "../checker/checker_log.txt";
-		std::ofstream log_file_;
-		std::string data_dir_path_ = "../test_generator/test_data/";
-		int count_tests_;
-
-		enum class log_params_ { NO = 1, VERDICT, MESSAGE, TIME, MEMORY };
-
-	private:
-		void write_type(log_params_ param){
-			log_file_ << static_cast<int>(param) << std::endl;
-		}
-		void write_verdict(verdict v){
-			//write_type(log_params_::VERDICT);
-			log_file_ << v;
-		}
-		void write_message(std::string message){
-			//write_type(log_params_::MESSAGE);
-			log_file_ << message;
-		}
 	public:
 
 		Checker(int count_tests){
@@ -40,5 +20,26 @@ class Checker{
 		~Checker(){
 			log_file_.close();
 		}
+	private:
+		void write_type(log_params_ param){
+			log_file_ << static_cast<int>(param) << std::endl;
+		}
+		void write_verdict(verdict v){
+			//write_type(log_params_::VERDICT);
+			log_file_ << v;
+		}
+		void write_message(std::string message){
+			//write_type(log_params_::MESSAGE);
+			log_file_ << message << "\n";
+
+		}
+	private:
+		const std::string log_file_path_ = "../checker/checker_log.txt";
+		std::ofstream log_file_;
+		std::string data_dir_path_ = "../test_generator/test_data/";
+		int count_tests_;
+
+		enum class log_params_ { NO = 1, VERDICT, MESSAGE, TIME, MEMORY };
+
 };
 #endif //SHELL_SORT_CHECKER_CHECKER_H 
