eb2e2cd0413eb6538ecc5b241abf2ad2f23b9848
tolik <talamtolik@gmail.com>
2018-03-02 07:06:13 +0300

    add to file-manager load func

--- src/file_manager.cpp
+++ src/file_manager.cpp
@@ -7,3 +7,18 @@ void FileManager::write(std::vector<int> data){
 
 	copy(data.begin(), data.end(), file_iterator);
 }
+
+std::vector<int> load(){
+	file_.open(file_path_, ios::in | ios::binary);
+
+    std::vector<int> data;
+
+	istream_iterator<int> begin_file(file_);
+	istream_iterator<int> end_file;
+
+	copy(begin_file, end_file, back_inserter(data));
+
+	file_.close();
+
+    return data;
+}
